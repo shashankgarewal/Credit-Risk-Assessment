c826dd8db16f736946e7ab506ae588f1216fe8a7
shashankgarewal <s.garewal@alumni.iitg.ac.in>
2025-02-03 11:58:57 +0530

    modified code for handling customer record

--- src/deploy.py
+++ src/deploy.py
@@ -1,6 +1,8 @@
+import json
 import pickle
+import random
 import numpy as np
-import pandas as pd
+import pandas as pd 
 import os.path as path
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.inspection import permutation_importance
@@ -8,16 +10,38 @@ from flask import Flask, render_template, request, jsonify
 from pipeline_components import Stage1Classifier, Stage2Classifier
 
 
-model = pickle.load(open(path.abspath(r'..\deliverables\model_v3.mdl'), 'rb')) # load model
-X, y = pickle.load(open(path.abspath(r'..\artifacts\test_data.pkl'), 'rb')) # load data
-#result = permutation_importance(model, X, y, scoring='accuracy')
+X, y = pickle.load(open(path.abspath(r'..\artifacts\test_data.pkl'), 'rb'))
+model = pickle.load(open(path.abspath(r'..\deliverables\model_v3.mdl'), 'rb')) 
 
-app = Flask(__name__) # flask instance
+# only P1-P3 model is used since the other models relies on the credit score, which has highest importance for p1-p3 also
+feature_imp = model.named_steps['stage2'].p1_p3_model.\
+                                                    get_booster().\
+                                                        get_score(importance_type='weight').items() 
+feature_imp_df = pd.DataFrame(feature_imp, columns=['features', 'importance'])\
+                        .sort_values(by='importance', ascending=False)
+
+feature_imp_list = feature_imp_df['features'].tolist()
+feature_range = {}
+feature_dtype = {}
+
+for feature in feature_imp_df['features'].values:
+    feature_range[feature] = f'{X[feature].min()} - {X[feature].max()}'
+    feature_dtype[feature] = str(X[feature].dtype.name)
+        
+# create a flask app
+app = Flask(__name__) 
+
+def single_predict(x):
+    return model.predict(x.to_frame().T)[0] + 1
 
 # route for homepage
 @app.route('/')
 def home():
-    return render_template('index.html', **locals())
+    return render_template('index.html', 
+                           feature_imp_list=feature_imp_list, 
+                           feature_range=feature_range, 
+                           feature_dtype=feature_dtype)
+        
 
 # route for predict
 
@@ -29,19 +53,34 @@ def predict():
         prospect_id = request.form.get('PROSPECTID')
         try:
             prospect_id = int(prospect_id)
-            result = 'P' + str(model.predict(X.loc[prospect_id].to_frame().T)[0] + 1)        
+            result = f'P{single_predict(X.loc[prospect_id])}'
         except:
-            result = 'invalid prospect_id'
-        return render_template('index.html', **locals())
+            result = 'invalid customer_id'
+        print('result: ', result)
+        return render_template('index.html', **locals(), 
+                               feature_imp_list=feature_imp_list, 
+                               feature_range=feature_range, 
+                               feature_dtype=feature_dtype)
     else:
-        record = []
+        index = random.choice(X.index)
+        temp = X.loc[index].copy()
         try:
-            for i in range(1, 40):
-                record.append(request.form)
-            result = model.predict([record])
+            for feature in feature_imp_df['features'].values:
+                feature_value = request.form.get(feature)
+                if feature_dtype[feature] == 'bool':
+                    temp[feature] = True if feature_value == 'on' else False
+                elif feature_value != "":
+                    temp[feature] = feature_value
+            result = f'P{single_predict(temp)}'
         except:
             result = 'invalid customer record'  
-        return render_template('index.html', **locals())
+        return render_template('index.html',
+                               **locals(),
+                               feature_imp_list=feature_imp_list, 
+                               feature_range=feature_range,
+                               feature_dtype=feature_dtype)
+
+#        return render_template('index.html', feature_range=json.dumps(feature_range))
 
 # Step 5: Run the Flask app
 if __name__ == '__main__':
